4a5eeb3ee1ae1f9a6e4bea180d440b73e56b1a6f
rafael731 <ryosha514@gmail.com>
2022-03-11 23:19:11 +1000

    -update some features

---
+++ js/typed.js
@@ -0,0 +1,9 @@
+  var typed = new Typed('#typed2', {
+    strings: ['Hi, I am Nikolay Marinov', 'Senior Full Stack Developer'],
+    typeSpeed: 50,
+    backSpeed: 2,
+    backDelay: 500,
+    startDelay: 100,
+    fadeOut: true,
+    loop: true
+  });
